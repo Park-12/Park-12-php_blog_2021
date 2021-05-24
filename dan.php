a0cd633a906a0b6b2aa610fe9677ad94f1dd6767
Park-12 <sohyunp96@gmail.com>
2021-05-24 19:09:31 +0900

    파라미터를 통해서 동적 구구단 생성기 PHP 완성

--- dan.php
+++ dan.php
@@ -1,5 +1,14 @@
 <?php
-$dan = 6
+if ( !isset($_GET['dan']) ) {
+  $_GET['dan'] = "2";
+}
+
+if ( !isset($_GET['limit']) ) {
+  $_GET['limit'] = "9";
+}
+
+$dan = intval($_GET['dan']);
+$limit = intval($_GET['limit']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,16 +24,24 @@ $dan = 6
   <h1><?=$dan?>단</h1>
 
   <hr>
-  <?php for ($i = 1; $i <=9; $i++) { ?>
+
+  <?php for ( $i = 1; $i <= $limit; $i++ ) { ?>
     <div>
-    <?= $dan?> * <?=$i?> = <?=$dan * $i?>
+      <?=$dan?> * <?=$i?> = <?=$dan * $i?>
     </div>
   <?php } ?>
-
-
+  
   <hr>
 
-  <a href="1단.html">1단</a>
-  <a href="2단.html">2단</a>
+  <?php for ( $dan2 = 1; $dan2 <= 9; $dan2++ ) { ?>
+    <?php
+    $aClass = "";
+
+    if ( $dan2 == $dan ) {
+      $aClass = "red";
+    }
+    ?>
+    <a class="<?=$aClass?>" href="dan.php?dan=<?=$dan2?>"><?=$dan2?>단</a>
+  <?php } ?>
 </body>
-</html> 
+</html>
