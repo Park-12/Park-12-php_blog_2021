d0db141e206acc9c14a8d44ddf7d8689d1760591
Park-12 <sohyunp96@gmail.com>
2021-06-01 15:49:14 +0900

    상단, 하단 공통 디자인 적용

--- usr/article/modify.php
+++ usr/article/modify.php
@@ -21,44 +21,35 @@ if ( $article == null) {
     exit;
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>게시물 수정, <?=$id?>번 게시물</title>
-</head>
-<body>
-    <h1>게시물 수정, <?=$id?>번 게시물</h1>
-    <hr>
-    
-    <div>
+<?php
+$pageTitle = "게시물 수정, <?=$id?>번 게시물";
+?>
+<?php require_once __DIR__ . "/../head.php"; ?>
+
+<div>
     <a href="list.php">리스트</a>
     <a href="detail.php?id=<?=$id?>">원문</a>
-    </div>
-    <hr>
-
-    <form action="doModify.php">
+</div>
+<hr>
+<form action="doModify.php">
     <input type="hidden" name="id" value="<?=$id?>">
     <div>
-    <span>번호</span>
-    <span><?=$article['id']?></span>
-    </div>
-
-    <span>제목</span>
-    <input required placeholder="제목을 입력해주세요." type="text" name="title" value="<?=$article['title']?>">
+        <span>번호</span>
+        <span><?=$article['id']?></span>
     </div>
 
     <div>
-    <span>내용</span>
-    <textarea required placeholder="내용을 입력해주세요." name="body"><?=$article['body']?></textarea>
+        <span>제목</span>
+        <input required placeholder="제목을 입력해주세요." type="text" name="title" value="<?=$article['title']?>">
     </div>
 
     <div>
-    <input type="submit" value = "글 수정">
+        <span>내용</span>
+        <textarea required placeholder="내용을 입력해주세요." name="body"><?=$article['body']?></textarea>
     </div>
-    </form>
 
-</body>
-</html>
+    <div>
+        <input type="submit" value = "글 수정">
+    </div>
+</form>
+<?php require_once __DIR__ . "/../foot.php"; ?> 
--- usr/article/write.php
+++ usr/article/write.php
@@ -1,35 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>게시물 작성</title>
-</head>
-<body>
-    <h1>게시물 작성</h1>
-
-    <div>
-    <a href="list.php">글 리스트</a>
-  </div>
-  <hr>
-  
-    <hr>
-
-    <form action="doWrite.php">
-    <div>
+<?php
+$pageTitle = "글작성";
+?>
+<?php require_once __DIR__ . "/../head.php"; ?>
+<div>
+  <a href="list.php">글 리스트</a>
+</div>
+<hr>
+<form action="doWrite.php">
+  <div>
     <span>제목</span>
     <input required placeholder="제목을 입력해주세요." type="text" name = "title">
-    </div>
+  </div>
 
-    <div>
+  <div>
     <span>내용</span>
     <textarea required placeholder="제목을 입력해주세요." name = "body"></textarea>
-    </div>
+  </div>
 
-    <div>
+  <div>
     <input type="submit" value = "글작성">
-    </div>
-    </form>
-</body>
-</html>
+  </div>
+</form>
+<?php require_once __DIR__ . "/../foot.php"; ?> 
