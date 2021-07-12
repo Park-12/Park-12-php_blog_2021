4d34ca32a0131bde8043031f309c06f7f4794f6f
Park-12 <sohyunp96@gmail.com>
2021-07-12 11:50:59 +0900

    로그인, 자기소개 페이지 여백조정

--- src/view/usr/home/aboutMe.php
+++ src/view/usr/home/aboutMe.php
@@ -1,5 +1,5 @@
 <?php
-$pageTitleIcon = '<i class="fas fa-list"></i>';
+$pageTitleIcon = '<i class="far fa-id-card"></i>';
 $pageTitle = "ABOUT ME";
 ?>
 <?php require_once __DIR__ . "/../head.php"; ?>
@@ -7,12 +7,14 @@ $pageTitle = "ABOUT ME";
 
 <section class="section-about-me">
     <div class="container mx-auto">
-        이름 :
-        <br>
-        블로그 :
-        <br>
-        취미 :
-        <br>
+        <div class="con-pad">
+            이름 :
+            <br>
+            블로그 :
+            <br>
+            취미 :
+            <br>
+        </div>
     </div>
 </section>
 <?php require_once __DIR__ . "/../foot.php"; ?>
